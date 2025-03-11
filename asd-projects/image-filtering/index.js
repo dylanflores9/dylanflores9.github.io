6112fab1718c462b20f7efaedd39474c31d912c2
thegoat911 <143840890+thegoat911@users.noreply.github.com>
2025-03-11 18:15:48 +0000

    skibidi sigma

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -4,56 +4,111 @@ $(document).ready(function () {
   render($("#display"), image);
   $("#apply").on("click", applyAndRender);
   $("#reset").on("click", resetAndRender);
-});
-
-/////////////////////////////////////////////////////////
-//////// event handler functions are below here /////////
-/////////////////////////////////////////////////////////
-
-// this function resets the image to its original value; do not change this function
-function resetAndRender() {
+ });
+ 
+ 
+ /////////////////////////////////////////////////////////
+ //////// event handler functions are below here /////////
+ /////////////////////////////////////////////////////////
+ 
+ 
+ // this function resets the image to its original value; do not change this function
+ function resetAndRender() {
   reset();
   render($("#display"), image);
-}
-
-// this function applies the filters to the image and is where you should call
-// all of your apply functions
-function applyAndRender() {
+ }
+ 
+ 
+ // this function applies the filters to the image and is where you should call
+ // all of your apply functions
+ function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter()
-
-
+ 
+ 
+  applyFilter(reddify)
+  applyFilterNoBackground(decreaseBlue)
+  applyFilterNoBackground(increaseGreenByBlue)
+ 
+ 
   // do not change the below line of code
   render($("#display"), image);
-}
-
-/////////////////////////////////////////////////////////
-// "apply" and "filter" functions should go below here //
-/////////////////////////////////////////////////////////
-
-// TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter() {
+ }
+ 
+ 
+ /////////////////////////////////////////////////////////
+ // "apply" and "filter" functions should go below here //
+ /////////////////////////////////////////////////////////
+ 
+ 
+ // TODO 1, 2 & 4: Create the applyFilter function here
+ function applyFilter(filterFunction) { // Apply's the new filter to the image.
   for (var i = 0; i < image.length; i++) {
     for (var j = 0; j < image[i].length; j++) {
       var rgbString = image[i][j]
-      var rgbNumber = rgbStringToArray(rgbString)
-      rgbNumber[RED] = 255
-      rgbString = rgbArrayToString(rgbNumber)
+      var rgbNumbers = rgbStringToArray(rgbString)
+      filterFunction(rgbNumbers)
+      rgbString = rgbArrayToString(rgbNumbers)
       image[i][j] = rgbString
     }
   }
-}
-
-// TODO 7: Create the applyFilterNoBackground function
-
-
-// TODO 5: Create the keepInBounds function
-
-
-// TODO 3: Create reddify function
-
-
-// TODO 6: Create more filter functions
-
-
-// CHALLENGE code goes below here
+ }
+ 
+ 
+ // TODO 7: Create the applyFilterNoBackground function
+ function applyFilterNoBackground(filterFunction) {
+  var backgroundColour = image[0][0]
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      if (image[i][j] === backgroundColour) {
+        console.log("yay")
+        var rgbString = image[i][j]
+        var rgbNumbers = rgbStringToArray(rgbString)
+        filterFunction(rgbNumbers)
+        rgbString = rgbArrayToString(rgbNumbers)
+        image[i][j] = rgbString
+      }
+ 
+ 
+    }
+  }
+ }
+ 
+ 
+ // TODO 5: Create thiltFe keepInBounds function
+ function keepInBounds(num1) {
+  if (num1 < 0) {
+    return 0
+  } else if (num1 > 255) {
+    return 255
+  } else {
+    return num1
+  }
+ 
+ 
+ }
+ 
+ 
+ // TODO 3: Create reddify function
+ function reddify(array1) { // Turns every RED value of each pixel to 200
+  array1[RED] = 200
+ 
+ 
+ }
+ 
+ 
+ // TODO 6: Create more filter functions
+ 
+ 
+ function decreaseBlue(array2) {
+  array2[BLUE] = keepInBounds(array2[BLUE] - 50)
+ 
+ 
+ }
+ function increaseGreenByBlue(array3) {
+  array3[GREEN] = keepInBounds(array3[BLUE] + array3[GREEN])
+ }
+ // CHALLENGE code goes below here
+ 
+ 
+ 
+ 
